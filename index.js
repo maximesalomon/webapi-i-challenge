d33ddad36d59609c9ff24442ab034708f7e7167e
Maxime Salomon <maxime@croissant.io>
2019-03-18 15:41:02 +0100

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+
+const express = require('express');
+
+const server = express();
+const db = require('./data/db')
+
+// ENDPOINTS
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then()
+    .catch()
+  res.status(200).json(users);
+});
+
+server.listen(8000, () => console.log('API running on port 8000'));
