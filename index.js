ba361038be54c33da035b2c78d33eef84e50f4ff
Maxime Salomon <maxime@croissant.io>
2019-03-18 16:32:34 +0100

    added error handling to post new user

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ server.use(parser);
 
 // ENDPOINTS
 
+// POST API/USERS
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    const { name, bio } = req.body;
+    db.insert(user)
+    .then((user) => {
+        if (name, bio) {
+            res.status(200).json(user);
+        } else {
+            res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+        }
+    })
+    .catch(err => {
+        res.status(500)
+        .json({ error: "There was an error while saving the user to the database" });
+    })
+});
+
 // GET API/USERS
 server.get('/api/users', (req, res) => {
     db.find()
@@ -36,19 +54,6 @@ server.get('/api/users/:id', (req, res) => {
     .catch(err => {
         res.status(500)
         .json({message: "failed to get user"});
-    })
-});
-
-// POST API/USERS
-server.post('/api/users', (req, res) => {
-    const user = req.body;
-    db.insert(user)
-    .then((user) => {
-            res.status(200).json(user);
-    })
-    .catch(err => {
-        res.status(500)
-        .json({message: "failed to insert user in db"});
     })
 });
 
